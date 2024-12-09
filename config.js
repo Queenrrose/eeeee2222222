f20af21a674a9e5fe3551426f2a0e2d61e0e6a0a
Queen Rose <00queen.rose@gmail.com>
2024-12-09 07:58:19 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  bot: {
+    // serverId: ["1025881641744351232"],
+    owners: ["831692613542608896" , "638084788472774697","948386383502901249","200147471576465408"],// owners id
+    botID: "867781907860946964",// bot id
+    serverinvte: `https://discord.gg/java1)`, // Server invite url
+    clientSECRET: "8WoG4qsiCHLypgoPKOdyGNdt-dKmGj8y", // Client Secret
+    callbackURL: "https://verfffff.ahmedhhhtht.repl.co/login", // Call Back Url
+    TOKEN: (process.env.token) // Bot Token
+  },
+  website: {
+    PORT: "3001",//website port
+  }
+}
